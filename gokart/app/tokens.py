5001d7eb483a68f72a8c94e41ec8ced650c354e6
elias <rustyverse@noreply.codeberg.org>
2025-03-21 20:45:51 +0100

    Code cleanup

--- gokart/app/tokens.py
+++ gokart/app/tokens.py
@@ -1,10 +1,11 @@
-# tokens.py
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
 
+
+# Generate password to prevent unauthorized people to cancel others bookings
 class BookingCancellationTokenGenerator(PasswordResetTokenGenerator):
     def _make_hash_value(self, booking, timestamp):
-        # Using str() directly instead of six
         return str(booking.id) + str(booking.customer.id) + str(timestamp)
+
 
 # Create an instance of the token generator
 booking_cancellation_token = BookingCancellationTokenGenerator()
--- gokart/app/views.py
+++ gokart/app/views.py
@@ -1,53 +1,68 @@
-from django.shortcuts import render, redirect, get_object_or_404, redirect
-from django.http import JsonResponse, HttpResponse
-from .models import Customer, Gokart, Booking, Drivers
-from datetime import time
 from django.contrib import messages
-import smtplib
+from django.http import JsonResponse, HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404, redirect
+from datetime import time
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-from django.conf import settings
 from .tokens import booking_cancellation_token
-from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
-from django.template.loader import render_to_string
-from django.contrib.auth.tokens import default_token_generator
+from .models import Customer, Gokart, Booking, Drivers
+import smtplib
 
 # Create your views here.
+
 
 def home(request):
     return render(request, "index.html")
 
+
 def success(request):
     return render(request, "success.html")
+
 
 def failed(request):
     return render(request, "failed.html")
 
+
 def data(request):
-    bookings = Booking.objects.prefetch_related('drivers_set__gokart').all().order_by('date', 'time')
-    return render(request, 'data.html', {'bookings': bookings})
+    bookings = (
+        Booking.objects.prefetch_related("drivers_set__gokart")
+        .all()
+        .order_by("date", "time")
+    )
+    return render(request, "data.html", {"bookings": bookings})
+
 
 def get_available_slots(request):
     date = request.GET.get("date")
 
     # Define possible time slots
-    all_slots = [time(10, 00), time(10, 30),
-                 time(11, 00), time(11, 30),
-                 time(13, 00), time(13, 30),
-                 time(14, 00), time(14, 30)]
+    all_slots = [
+        time(10, 00),
+        time(10, 30),
+        time(11, 00),
+        time(11, 30),
+        time(13, 00),
+        time(13, 30),
+        time(14, 00),
+        time(14, 30),
+    ]
 
     # Get already booked slots for the selected date
     booked_slots = Booking.objects.filter(date=date).values_list("time", flat=True)
 
     # Filter out booked slots
-    available_slots = [slot.strftime("%H:%M") for slot in all_slots if slot not in booked_slots]
+    available_slots = [
+        slot.strftime("%H:%M") for slot in all_slots if slot not in booked_slots
+    ]
 
     return JsonResponse({"slots": available_slots})
+
 
 def get_gokarts(request):
     gokarts = Gokart.objects.all()
     gokart_list = [{"id": gokart.id, "name": gokart.name} for gokart in gokarts]
     return JsonResponse(gokart_list, safe=False)
+
 
 def book_slot(request):
     if request.method == "POST":
@@ -58,16 +73,22 @@ def book_slot(request):
         num_people = int(request.POST.get("num_people"))  # Number of people
 
         if time is None or time == 0 or time == "":
-            messages.error(request, "Ingen vald tid hittad för bokningen. Vänligen försök igen.")
+            messages.error(
+                request, "Ingen vald tid hittad för bokningen. Vänligen försök igen."
+            )
             return redirect("failed")
 
         # Check if the slot is available
         if Booking.objects.filter(date=date, time=time).exists():
-            messages.error(request, "Tiden du angav är tyvärr redan uppbokad, vänligen försök igen med en annan tid.")
+            messages.error(
+                request,
+                "Tiden du angav är tyvärr redan uppbokad, vänligen försök igen med en annan tid.",
+            )
             return redirect("failed")
-            #return JsonResponse({"error": "Slot already booked"}, status=400)
 
-        customer, created = Customer.objects.get_or_create(email=email, defaults={'name': name})
+        customer, created = Customer.objects.get_or_create(
+            email=email, defaults={"name": name}
+        )
 
         # Create a new booking instance, but don't save it yet
         booking = Booking.objects.create(date=date, time=time, customer=customer)
@@ -81,9 +102,13 @@ def book_slot(request):
         send_booking_confirmation_mail(booking, name, email, date, time)
 
         # Display a success message
-        messages.success(request, 'Din bokning har registrerats. Vi har skickat tid, datum och kvitto via epost. Vi ses snart.')
+        messages.success(
+            request,
+            "Din bokning har registrerats. Vi har skickat tid, datum och kvitto via epost. Vi ses snart.",
+        )
         return redirect("success")
-        #return JsonResponse({"success": "Booking confirmed"})
+        # return JsonResponse({"success": "Booking confirmed"})
+
 
 def cancel_booking(request):
     if request.method == "POST":
@@ -100,59 +125,58 @@ def cancel_booking(request):
 
     return HttpResponse("Invalid request", status=400)
 
+
 def get_object_or_none(model, **kwargs):
     try:
         return model.objects.get(**kwargs)
     except model.DoesNotExist:
         return None
 
+
 def cancel_booking_from_email_link(request, booking_id, token):
     # Fetch the booking by ID
     booking = get_object_or_none(Booking, id=booking_id)
     if booking is None:
-        # Invalid booking
-        messages.error(request, 'You have already canceled your booking.')
-        return redirect('failed')
+        # Booking already canceled
+        messages.error(request, "Den är bokningen är redan avbokad.")
+        return redirect("failed")
 
     # Verify the token
     if booking_cancellation_token.check_token(booking, token):
         # Token is valid, proceed with cancellation
         booking.delete()
 
-        # Display a success message
-        messages.success(request, 'Your booking has been successfully cancelled.')
-        return redirect('success')
+        messages.success(request, "Din bokning är nu avbokad.")
+        return redirect("success")
     else:
         # Invalid token
-        messages.error(request, 'The cancellation link is invalid or has expired.')
-        return redirect('failed')
+        messages.error(request, "Den här avbokningslänken är ogiltig.")
+        return redirect("failed")
+
 
 # We are using https://app.mailersend.com as our SMTP host provider.
 def send_booking_confirmation_mail(booking, name, mail, date, time):
     # Generate the token for the booking
     token = booking_cancellation_token.make_token(booking)
-    # Create the cancellation link with the token
+
+    # Website URL hardcoded. Probably a better way of doing this.
     cancellation_link = f"http://127.0.0.1:8000/cancel_booking/{booking.id}/{token}/"
 
-    # Email details
     sender_email = "MS_62DvdC@trial-yzkq340vvvkld796.mlsender.net"
     receiver_email = mail
     password = "mssp.PyDKoSt.7dnvo4deo3rl5r86.NHJoMli"
     subject = "Gokart booking confirmation"
-    body = f"Hello {name},\nWe have received a gokart booking for you on the {date} at {time}.\nHope to see you there.\nWould you like to cancel your booking?\nLink: {cancellation_link}"
+    body = f"Hej {name},\n\nVi bekräftar härmed din gokartbokning den {date} kl. {time}.\n\nVi ser fram emot att välkomna dig vid detta tillfälle. Om du önskar att avboka din bokning, vänligen klicka på följande länk: {cancellation_link}.\n\nMed vänliga hälsningar,\nGokart-Skolprojekt AB"
 
-    # Create the email message
     message = MIMEMultipart()
     message["From"] = sender_email
     message["To"] = receiver_email
     message["Subject"] = subject
     message.attach(MIMEText(body, "plain"))
 
-    # Set up the SMTP server
     smtp_server = "smtp.mailersend.net"
     port = 587
 
-    # Send the email
     with smtplib.SMTP(smtp_server, port) as server:
         server.starttls()
         server.login(sender_email, password)
